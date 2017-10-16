25d2304e505fc25efe4dd2d8bbf30f2805258e56
xiaodong.yan <xyan@adobe.com>
2017-10-16 17:02:13 +0800

    update one line and reformat the code based on PEP-8 to make it more readable

--- perceptron.py
+++ perceptron.py
@@ -9,7 +9,7 @@ class Perceptron(object):
         '''
         self.activator = activator
         # 权重向量初始化为0
-        self.weights = [0.0 for _ in range(input_num)]
+        self.weights = [0.0] * input_num
         # 偏置项初始化为0
         self.bias = 0.0
 
@@ -19,7 +19,6 @@ class Perceptron(object):
         '''
         return 'weights\t:%s\nbias\t:%f\n' % (self.weights, self.bias)
 
-    
     def predict(self, input_vec):
         '''
         输入向量，输出感知器的计算结果
@@ -30,10 +29,10 @@ class Perceptron(object):
         # 最后利用reduce求和
         return self.activator(
             reduce(lambda a, b: a + b,
-                   map(lambda (x, w): x * w,  
+                   map(lambda (x, w): x * w,
                        zip(input_vec, self.weights))
-                , 0.0) + self.bias)
- 
+                   , 0.0) + self.bias)
+
     def train(self, input_vecs, labels, iteration, rate):
         '''
         输入训练数据：一组向量、与每个向量对应的label；以及训练轮数、学习率
@@ -83,11 +82,11 @@ def get_training_dataset():
     '''
     # 构建训练数据
     # 输入向量列表
-    input_vecs = [[1,1], [0,0], [1,0], [0,1]]
+    input_vecs = [[1, 1], [0, 0], [1, 0], [0, 1]]
     # 期望的输出列表，注意要与输入一一对应
     # [1,1] -> 1, [0,0] -> 0, [1,0] -> 0, [0,1] -> 0
     labels = [1, 0, 0, 0]
-    return input_vecs, labels    
+    return input_vecs, labels
 
 
 def train_and_perceptron():
@@ -99,11 +98,11 @@ def train_and_perceptron():
     # 训练，迭代10轮, 学习速率为0.1
     input_vecs, labels = get_training_dataset()
     p.train(input_vecs, labels, 10, 0.1)
-    #返回训练好的感知器
+    # 返回训练好的感知器
     return p
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     # 训练and感知器
     and_perception = train_and_perceptron()
     # 打印训练获得的权重
@@ -113,5 +112,3 @@ if __name__ == '__main__':
     print '0 and 0 = %d' % and_perception.predict([0, 0])
     print '1 and 0 = %d' % and_perception.predict([1, 0])
     print '0 and 1 = %d' % and_perception.predict([0, 1])
-
-
